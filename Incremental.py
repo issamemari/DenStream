3ad4d831de85a63af58c34ddf2fe64e985a813c3
issamemari <issa.memari@gmail.com>
2018-08-09 16:17:35 +0200

    Implement weighted incremental standard deviation

---
+++ Incremental.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+class WeightedIncrementalStandardDeviation:
+
+    def __init__(self):
+
+        self.decay_factor = 1
+        self.mean = 0
+        self.variance_acc = 0
+        self.sum_of_weights = 0
+
+    def update(self, sample, weight):
+
+        print(
+            f"Current std is {self.std()}. Current mean is {self.mean}. Updating std with new sample {sample} with weight {weight}.")
+        if self.sum_of_weights != 0:
+
+            # Update sum of weights
+            old_sum_of_weights = self.sum_of_weights
+            new_sum_of_weights = old_sum_of_weights * self.decay_factor + weight
+
+            # Update mean
+            old_mean = self.mean
+            new_mean = old_mean + \
+                (weight / new_sum_of_weights) * (sample - old_mean)
+
+            # Update variance
+            old_variance_acc = self.variance_acc
+            new_variance_acc = old_variance_acc * ((new_sum_of_weights - weight)
+                                                   / old_sum_of_weights) \
+                + weight * (sample - new_mean) * (sample - old_mean)
+
+            self.mean = new_mean
+            self.variance_acc = new_variance_acc
+            self.sum_of_weights = new_sum_of_weights
+
+        else:
+            self.mean = sample
+            self.variance_acc = 0
+            self.sum_of_weights = weight
+
+    def std(self):
+        if self.sum_of_weights > 0:
+            return np.sqrt(self.variance_acc / self.sum_of_weights)
+        else:
+            return float('nan')
+
+std = WeightedIncrementalStandardDeviation()
+for i in range(100):
+    std.update(i, 1)
+
+print(std.std())
+print(std.mean)
+
+print(list(range(100)))
